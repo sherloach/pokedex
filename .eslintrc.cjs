4370af60932b21d045d0cf35480b0267e3a56692
Hoa Ngo <hoango@nexondv.com>
2022-11-29 14:20:40 +0700

    feat: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,5 +24,6 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/function-component-definition': 0,
     'no-restricted-exports': 0,
+    'import/prefer-default-export': 0,
   },
 };
