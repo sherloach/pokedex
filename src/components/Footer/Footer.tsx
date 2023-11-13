9808ad948a6d01259211e39ef0e50778d941b663
Hayden Ngo <hoango1607@gmail.com>
2023-11-13 15:34:56 +1030

    chore: add space

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <div className="main-container">
       <footer className="border-t border-solid border-[#24293f] py-8">
         <p className="text-center text-sm text-white">
-          Image rights to Nintendo & The Pokémon Company Data taken from API -
-          pokeapi.co
+          Image rights to Nintendo & The Pokémon Company Data taken from API -{' '}
+          <a href="https://pokeapi.co/">pokeapi.co</a>
         </p>
       </footer>
     </div>
